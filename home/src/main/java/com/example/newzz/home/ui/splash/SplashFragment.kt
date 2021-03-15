e916294c86092a8568f471a6145e2350074dbcb1
Darryll Aeria <darryllaeria@gmail.com>
2021-03-15 17:31:10 +0700

    Added view binding to project

--- home/src/main/java/com/example/newzz/home/ui/splash/SplashFragment.kt
+++ home/src/main/java/com/example/newzz/home/ui/splash/SplashFragment.kt
@@ -6,9 +6,8 @@ import com.example.newzz.home.R
 import com.example.newzz.base.base_component.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class SplashFragment: BaseFragment() {
+class SplashFragment: BaseFragment(R.layout.fragment_splash) {
 
-    override var layoutId = R.layout.fragment_splash
     private val viewModel: SplashViewModel by viewModel()
 
     override fun initLogic() {
