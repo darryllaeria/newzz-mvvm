a4c84eefab1a082ef3ebb1c36dff403f5ad7eb7a
Darryll Aeria <darryllaeria@gmail.com>
2021-03-15 10:09:24 +0700

    Renamed auth module to home module

--- auth/src/main/java/com/example/newzz/authentication/AuthModule.kt
+++
@@ -1,9 +0,0 @@
-package com.example.newzz.authentication
-
-import com.example.newzz.authentication.ui.splash.SplashViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.dsl.module
-
-val authModule = module {
-    viewModel { SplashViewModel() }
-}
--- home/src/test/java/com/example/newzz/home/ExampleUnitTest.kt
+++ home/src/test/java/com/example/newzz/home/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.newzz.authentication
+package com.example.newzz.home
 
 import org.junit.Test
 import org.junit.Assert.*
