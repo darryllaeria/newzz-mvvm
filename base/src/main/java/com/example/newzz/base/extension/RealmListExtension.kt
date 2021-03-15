682436e477524b3b7d8a3740d1ba6e7d7b0d9a04
Darryll Aeria <darryllaeria@gmail.com>
2021-03-15 09:33:03 +0700

    Temporarily remove Realm dependency in project, to be replaced with Room

--- base/src/main/java/com/example/newzz/base/extension/RealmListExtension.kt
+++ base/src/main/java/com/example/newzz/base/extension/RealmListExtension.kt
@@ -1,10 +1,10 @@
 package com.example.newzz.base.extension
 
-import io.realm.RealmList
-
-// MARK: - RealmList
-fun <T> RealmList<T>.convertToList(): List<T> {
-    val list: MutableList<T> = mutableListOf()
-    for (i in indices) { this[i]?.let { list.add(it) } }
-    return list
-}
+//import io.realm.RealmList
+//
+//// MARK: - RealmList
+//fun <T> RealmList<T>.convertToList(): List<T> {
+//    val list: MutableList<T> = mutableListOf()
+//    for (i in indices) { this[i]?.let { list.add(it) } }
+//    return list
+//}
