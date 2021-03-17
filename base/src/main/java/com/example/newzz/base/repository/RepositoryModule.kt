a855b4c3a9d66da9a488e93a94caebfcf081066b
Darryll Aeria <darryllaeria@gmail.com>
2021-03-17 12:56:19 +0700

    Fix import issue with some files

--- base/src/main/java/com/example/newzz/base/repository/RepositoryModule.kt
+++ base/src/main/java/com/example/newzz/base/repository/RepositoryModule.kt
@@ -1,9 +1,6 @@
 package com.example.newzz.base.repository
 
-import com.example.newzz.base.repository.device.DeviceRepositoryImpl
-import com.example.newzz.base.repository.device.IDeviceRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    single<IDeviceRepository> { DeviceRepositoryImpl(get(), get(), get()) }
 }
