5d1555cc7ae7276b25909402e2c0d976fbf950a7
Darryll Aeria <darryllaeria@gmail.com>
2021-03-18 11:51:05 +0700

    Modified news repository

--- base/src/main/java/com/example/newzz/base/repository/RepositoryModule.kt
+++ base/src/main/java/com/example/newzz/base/repository/RepositoryModule.kt
@@ -5,5 +5,5 @@ import com.example.newzz.base.repository.news.NewsRepositoryImpl
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    single<INewsRepository> { NewsRepositoryImpl(get()) }
+    single<INewsRepository> { NewsRepositoryImpl(get(), get()) }
 }
--- base/src/main/java/com/example/newzz/base/repository/news/NewsRepositoryImpl.kt
+++ base/src/main/java/com/example/newzz/base/repository/news/NewsRepositoryImpl.kt
@@ -1,23 +1,48 @@
 package com.example.newzz.base.repository.news
 
 import com.example.newzz.base.BuildConfig
-import com.example.newzz.base.data.model.NewsItemModel
+import com.example.newzz.base.data.database.dao.NewsDao
 import com.example.newzz.base.data.remote.NewsService
-import retrofit2.Response
+import com.example.newzz.base.utils.AppLog
+import kotlinx.coroutines.runBlocking
+import org.json.JSONArray
+import org.json.JSONObject
+
 
 class NewsRepositoryImpl(
-    private val newsService: NewsService
+    private val newsService: NewsService,
+    private val newsDao: NewsDao
 ): INewsRepository {
 
-    /**
-     *  @Detail Get news items
-     */
-    override suspend fun loadNews(q: String?, from: String?, sortBy: String?): Response<NewsItemModel> {
-        return newsService.getNewsItems(
-            q,
-            from,
-            sortBy,
-            BuildConfig.NEWS_API_KEY
-        )
+    // MARK: - Companion Object
+    companion object {
+        private val TAG: String = NewsRepositoryImpl::class.java.simpleName
     }
+
+    override suspend fun loadNews(q: String?, from: String?, sortBy: String?): Boolean {
+        TODO("Not yet implemented")
+        // Dummy
+    }
+
+//    override fun saveNewsJsons(jsonArray: JSONArray): Boolean {
+//        return try {
+//            runBlocking {
+//                val ids = newsDao.saveJSONDatas(jsonArray)
+//                val dataSize = jsonArray.length()
+//                dataSize != 0 && ids.size == dataSize
+//            }
+//        } catch (e: Exception) {
+//            AppLog.d(TAG, "Store user failed with exception: $e")
+//            false
+//        }
+//    }
+//
+//    /**
+//     *  @Detail Get news items
+//     */
+//    override suspend fun loadNews(q: String?, from: String?, sortBy: String?): Boolean {
+//        val jsonObject = JSONObject(newsService.getNewsItems(q, from, sortBy, BuildConfig.NEWS_API_KEY).string().trim())
+//        val jsonArray = jsonObject.getJSONArray("articles")
+//        return saveNewsJsons(jsonArray)
+//    }
 }
